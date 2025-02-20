cf172043259ece74abdc68d06cdf017fd5674540
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2025-02-20 20:39:56 +0530

    Create Find median in a stream.cpp

---
+++ Find median in a stream.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<double> getMedian(vector<int> &arr) {
+        vector<double> medians;
+        priority_queue<int> maxHeap; // Max heap for left half
+        priority_queue<int, vector<int>, greater<int>> minHeap; // Min heap for right half
+        
+        for (int num : arr) {
+            if (maxHeap.empty() || num <= maxHeap.top()) {
+                maxHeap.push(num);
+            } else {
+                minHeap.push(num);
+            }
+            
+            // Balance the heaps
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+            } else if (minHeap.size() > maxHeap.size()) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            }
+            
+            // Get the median
+            if (maxHeap.size() == minHeap.size()) {
+                medians.push_back((maxHeap.top() + minHeap.top()) / 2.0);
+            } else {
+                medians.push_back(maxHeap.top());
+            }
+        }
+        
+        return medians;
+    }
+};
